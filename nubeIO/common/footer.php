20519c76574dbfb7cde8abf32e4270159dff44a6
Ethan Dunne <32659387+riptho@users.noreply.github.com>
2018-07-13 16:29:59 +1000

    Updated Footer
    Removed Contact Form

--- nubeIO/common/footer.php
+++ nubeIO/common/footer.php
@@ -11,33 +11,6 @@
 								<li><i class="fa fa-envelope-o"></i><a href="mailto:info@nubeio.com.au">info@nube-io.com</a></li>
 								<li><i class="fa fa-linkedin-square"></i><a href="https://www.linkedin.com/company/nube-io/">Follow us on LinkedIn</a></li>
 							</ul>
-						
-						</div>
-					</div>
-					<div class="offset-lg-1 offset-md-1 col-lg-6 col-md-6 col-sm-12">
-						<div class="contact-form">
-							<h3 class="section-title">Send Us a Message</h3>
-							<div class="row">
-								<div class="col-md-6">
-									<div class="form-element">
-										<input type="text" placeholder="First & Last Name">
-									</div>
-								</div>
-								<div class="col-md-6">
-									<div class="form-element">
-										<input type="email" placeholder="E-Mail">
-									</div>
-								</div>
-							</div>
-							<div class="form-element">
-								<input placeholder="Subject Line">
-							</div>
-							<div class="form-element">
-								<textarea placeholder="Message"></textarea>
-							</div>
-							<div class="form-element">
-								<button class="btn-secondary-box">Submit</button>
-							</div>
 						</div>
 					</div>
 				</div>
